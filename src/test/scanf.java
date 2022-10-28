25553ab7a27bb04a707c74fc3f94c7a58865d9e6
Vu <vuquangle97@gmail.com>
2022-10-28 23:07:39 +0700

    commit two

---
+++ src/test/scanf.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class scanf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a; int b;
+		Scanner sc =  new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+
+}
