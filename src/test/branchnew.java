7345066f05835e53cd920599ff8eb11fc75f42d0
Vu <vuquangle97@gmail.com>
2022-10-28 23:14:31 +0700

    commitbranchnew

---
+++ src/test/branchnew.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class branchnew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a; int b;
+		Scanner sc =  new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+	}
+
+
